0f9d52cd3918cd9e618b9ebe29ce39904c717ef0
thoalt <161549287+mrmat1@users.noreply.github.com>
2024-03-09 08:13:23 +0700

    fix rpc

--- client/src/utils/Cons.jsx
+++ client/src/utils/Cons.jsx
@@ -1,7 +1,7 @@
 export const API_URL = "https://api.duongnm.online";
 export const API_GET_RPCS = API_URL + "/api/statusRpcs";
 export const API_GET_INDEXERS = API_URL + "/api/statusIndexers";
-export const RPC_URL = "https://rpc-namada.theamsolutions.info";
+export const RPC_URL = "https://namada-se-rpc.cosmicvalidator.com";
 export const RPC_STATUS = RPC_URL + "/status";
 
 export const TIME_RELOAD = 60000;
